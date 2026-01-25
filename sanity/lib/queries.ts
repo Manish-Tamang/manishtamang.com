b1e31562ea7417876081436d14f0af9770230b4e
Manish Tamang <maneshtamang833@gmail.com>
2026-01-25 22:53:44 +0545

    feat: Add Sanity queries for fetching all posts, a single post by slug, and featured posts.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -17,3 +17,11 @@ export const POST_BY_SLUG_QUERY = defineQuery(`*[_type == "post" && slug.current
     coverImage,
     content
 }`);
+export const FEATURED_POSTS_QUERY = defineQuery(`*[_type == "post"] {
+    title,
+    "slug": slug.current,
+    excerpt,
+    date,
+    coverImage,
+    "tag": category[0]
+} | order(date desc)[0...3]`);
