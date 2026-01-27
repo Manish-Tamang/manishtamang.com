8c7f3be8493f843adf8db69926c0b5dbfccd3692
Manish Tamang <maneshtamang833@gmail.com>
2026-01-27 21:54:52 +0545

    feat: enhance ManWhoCantBeMoved component with improved layout and responsive design

--- components/man-who-cant-be-moved.tsx
+++ components/man-who-cant-be-moved.tsx
@@ -4,13 +4,13 @@ import Image from "next/image"
 
 export function ManWhoCantBeMoved() {
     return (
-        <section className="w-[610px] mt-4 mb-6 grid grid-cols-2 items-center gap-6">
-            <p className="font-myfont text-[#636363] rotate-350 text-5xl">
+        <section className="max-w-[610px] w-full mx-auto mt-4 mb-6 grid grid-cols-2 items-center gap-4 md:gap-8 px-6 md:px-0">
+            <p className="font-myfont text-[#636363] dark:text-zinc-400 rotate-[350deg] text-3xl sm:text-4xl md:text-5xl leading-tight select-none">
                 If you see this girl,<br />
                 can you tell her <br />
                 where I am? :)
             </p>
-            <div className="relative w-full rotate-5 h-[260px]">
+            <div className="relative w-full rotate-[5deg] h-[150px] sm:h-[200px] md:h-[260px]">
                 <Image
                     src="/images/this-girl.png"
                     alt="Girl"
@@ -20,7 +20,6 @@ export function ManWhoCantBeMoved() {
                     priority
                 />
             </div>
-            {/* <p className="text-[#636363] text-[10px]" >The man who can't be moved - The Script</p> */}
         </section>
     )
 }
