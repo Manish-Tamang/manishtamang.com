f2b8462b5e469c02ed19f9ffc32c78bde253baaa
Manish Tamang <maneshtamang833@gmail.com>
2026-02-09 23:00:43 +0545

    feat(schema): add additional schema types for projects, favorites, images, timeline, socials, and about profile

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,13 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { post } from './post'
+import { project } from './project'
+import { featuredProjects } from './featuredProjects'
+import { recentFavorite } from './recentFavorite'
+import { featuredImage } from './featuredImage'
+import { timeline } from './timeline'
+import { social } from './social'
+import { aboutProfile } from './aboutProfile'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post],
+  types: [post, project, featuredProjects, recentFavorite, featuredImage, timeline, social, aboutProfile],
 }
