65b7a7f4e7bb60bdb835347c388b1a366d417230
Prakash-Tamang1 <maneshtamang833@gmail.com>
2026-03-06 01:37:12 +0545

    fix: add defer attribute to script tag for improved loading performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3993510219762880"
           crossOrigin="anonymous"
+          defer
         ></script>
       </head>
       <body
