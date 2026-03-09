59637b80303bf19cbf65c1d978fc85e70eff5780
Prakash-Tamang1 <maneshtamang833@gmail.com>
2026-03-10 00:04:28 +0545

    feat: add BlogPostStats interface to define blog post statistics structure

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export interface BlogPostStats {
+  slug: string;
+  title: string;
+  views: number;
+  thumbsUp: number;
+  heart: number;
+  trophy: number;
+  bookmark: number;
+  totalReactions: number;
+}
