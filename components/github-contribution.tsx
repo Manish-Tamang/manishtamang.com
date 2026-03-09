5587178f3537e66491eb658321cf76830903a3ba
Prakash-Tamang1 <maneshtamang833@gmail.com>
2026-03-10 00:03:14 +0545

    feat: add GithubContribution component to display GitHub contributions graph

---
+++ components/github-contribution.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import GithubContributionGraph from '@/components/GithubContributionGraph'
+
+export default function GithubContribution() {
+    return (
+        <section className="w-full">
+            <div className="w-full overflow-x-auto md:overflow-visible">
+                <div className="w-max md:w-auto">
+                    <GithubContributionGraph
+                        username="Manish-Tamang"
+                        apiBaseUrl="https://github-contributions-api.jogruber.de"
+                        rows={7}
+                        columns={52}
+                        theme="green"
+                        enableTooltip={true}
+                        tileStyles={{
+                            width: 9,
+                            height: 9,
+                        }}
+                        gridStyles={{
+                            maxWidth: "100%",
+                            width: "fit-content",
+                            overflow: "visible"
+                        }}
+                    />
+                </div>
+            </div>
+        </section>
+    )
+}
