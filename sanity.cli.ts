05290c3e3c005c91e5433c5d642afd6000af2066
Prakash-Tamang1 <maneshtamang833@gmail.com>
2026-03-16 21:05:33 +0545

    fix: update projectId and dataset defaults in Sanity CLI configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,12 +4,11 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "dtlyikoh"
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
 
 export default defineCliConfig({
     api: { projectId, dataset }, deployment: {
-        appId: 'f5gtqzef2yqumyqs3wgk6mbx',
-
-    }
+    appId: 'yuc1ycozh8j51g47kdhg0d1f',
+  }
 })
